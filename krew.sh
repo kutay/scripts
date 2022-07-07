5b8ce510e138e49c6fa34b9f775a2a133eaf88f3
Aykut Açikel <1253335+kutay@users.noreply.github.com>
2022-07-07 17:00:40 +0200

    Create krew.sh

---
+++ krew.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+
+
+kubectl krew install ctx
+kubectl krew install ns
+kubectl krew install doctor
+kubectl krew install popeye
+kubectl krew install fleet
+kubectl krew install np-viewer
+kubectl krew install pod-dive
+kubectl krew install topology
+kubectl krew install graph
+kubectl krew install lineage
+kubectl krew install viewnode
+kubectl krew install view-secret
+kubectl krew install cyclonus
+kubectl krew install ice
+kubectl krew install images
+kubectl krew install ingress-nginx
+kubectl krew install service-tree
+kubectl krew install whoami
+kubectl krew install vpa-recommendation
+kubectl krew install tap
+kubectl krew install outdated 
+kubectl krew install resource-capacity
